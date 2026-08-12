607650028f53cd708c3ef49da8d518c74e30fc7a
PlayIntegrityFix: Phantom Edition by FATEH <159710616+FBIVIP@users.noreply.github.com>
2026-08-12 13:33:34 +0000

    fateh7 v1.0

--- third_party/frameworks-native/opengl/tools/glgen/stubs/egl/eglGetNativeClientBufferANDROID.cpp
+++
@@ -1,6 +0,0 @@
-/* EGLClientBuffer eglGetNativeClientBufferANDROID ( struct AHardwareBuffer const *buffer ) */
-static jlong android_eglGetNativeClientBufferANDROID(JNIEnv* _env, jobject, jobject buffer) {
-    auto ahb = android::android_hardware_HardwareBuffer_getNativeHardwareBuffer(_env, buffer);
-    return reinterpret_cast<jlong>(eglGetNativeClientBufferANDROID(ahb));
-}
-
